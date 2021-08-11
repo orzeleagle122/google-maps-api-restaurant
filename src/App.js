f92585cb764fe62c798d6ae347d6a1606607dc2a
Patryk Orłowski <orzeleagle122@gmail.com>
2021-08-11 11:43:33 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+import { CssBaseline, Grid } from "@material-ui/core";
 
-const App=()=> {
+const App = () => {
   return (
-    <div className="App">
-          test
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+
+      <PlaceDetails />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete>
+            <div classname={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+                <InputBase
+                  placeholder="Search..."
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                />
+              </div>
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
